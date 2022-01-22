ab28f55701714235b41a06da78f9f5cf7fc2bc79
Daehwan Nam <dhnam@postech.ac.kr>
2022-01-22 14:06:25 +0900

    emacs-like bining update for insert mode

--- config.py
+++ config.py
@@ -38,3 +38,32 @@ config.bind("<ctrl+g>", 'mode-leave', mode='command')
 config.bind("<ctrl+g>", 'mode-leave', mode='hint')
 config.bind("<ctrl+g>", 'mode-leave', mode='caret')
 config.bind("<ctrl+g>", 'mode-leave', mode='prompt')
+
+# emacs binding for insert mode
+# https://gist.github.com/jumper047/ee821f789cd336b1105309f3ebf44f70
+c.bindings.commands['insert'].update({
+    # editing
+    '<ctrl-f>': 'fake-key <Right>',
+    '<ctrl-b>': 'fake-key <Left>',
+    '<ctrl-a>': 'fake-key <Home>',
+    '<ctrl-e>': 'fake-key <End>',
+    '<ctrl-n>': 'fake-key <Down>',
+    '<ctrl-p>': 'fake-key <Up>',
+    '<alt-f>': 'fake-key <Ctrl-Right>',
+    '<alt-b>': 'fake-key <Ctrl-Left>',
+    '<ctrl-d>': 'fake-key <Delete>',
+    '<alt-d>': 'fake-key <Ctrl-Delete>',
+    '<alt-backspace>': 'fake-key <Ctrl-Backspace>',
+    '<ctrl-w>': 'fake-key <Ctrl-x>',
+    '<ctrl-y>': 'insert-text {primary}',
+    '<ctrl-g>': 'mode-leave',
+
+    '<ctrl-shift-f>': 'fake-key <Shift-Right>',
+    '<ctrl-shift-b>': 'fake-key <Shift-Left>',
+    '<alt-shift-f>': 'fake-key <Ctrl-Shift-Right>',
+    '<alt-shift-b>': 'fake-key <Ctrl-Shift-Left>',
+
+    '<ctrl-k>': "fake-key '<Ctrl-Shift-End>' ;; fake-key '<Ctrl-x>'",
+    '<ctrl-/>': 'fake-key <Ctrl-z>',
+    '<ctrl-r>': 'fake-key <Ctrl-y>'
+})
